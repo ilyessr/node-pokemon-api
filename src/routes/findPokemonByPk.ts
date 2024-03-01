2a4365b213c816b1cd1828a4bea6d2bbdd1b52a1
Ilyess <rachedi.ilyess@gmail.com>
2024-03-01 14:57:25 +0100

    Add check

--- src/routes/findPokemonByPk.ts
+++ src/routes/findPokemonByPk.ts
@@ -6,6 +6,12 @@ export default (app: express.Application) => {
   app.get("/api/pokemons/:id", auth, (req: Request, res: Response) => {
     Pokemon.findByPk(req.params.id)
       .then((pokemon) => {
+        if (!pokemon) {
+          const message = "Le pokémon correspondant à cet ID n'a pas été trouvé.";
+          res.status(404).json({ message });
+          return;
+        }
+
         const message = "Un pokémon a bien été trouvé.";
         res.json({ message, data: pokemon });
       })
