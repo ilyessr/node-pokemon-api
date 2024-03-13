03c53ded77ba76aa5d2165949b923337ebfc26ce
Ilyess <rachedi.ilyess@gmail.com>
2024-03-13 18:30:21 +0100

    fix

--- src/routes/resetDB.ts
+++ src/routes/resetDB.ts
@@ -2,14 +2,10 @@ import express from "express";
 import { initDb } from "../db/sequelize";
 
 export default (app: express.Application) => {
-    app.post("/reset-db", async (req, res) => {
+    app.post("/api/reset-db", async (req, res) => {
         try {
             await initDb();
-            res
-                .status(200)
-                .send(
-                    "La base de données a été réinitialisée et repeuplée avec succès."
-                );
+            res.status(200).json({ message: "La base de données a été réinitialisée et repeuplée avec succès.", data: null });
         } catch (error) {
             const message =
                 "Erreur lors de la réinitialisation de la base de données.";
